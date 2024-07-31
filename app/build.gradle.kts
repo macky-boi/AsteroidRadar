6bed99698113659ae8ef5817c61209919ea2acdf
macky-boi <125427131+macky-boi@users.noreply.github.com>
2024-07-31 17:04:05 +0800

    add dependency: retrofit, retrofir-scalar using .toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter)
 }
