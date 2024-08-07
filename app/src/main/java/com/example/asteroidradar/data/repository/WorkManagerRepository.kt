7f3ea16d6f2e81ff851b27f10339e304cb87d68a
macky-boi <125427131+macky-boi@users.noreply.github.com>
2024-08-07 21:44:17 +0800

    add back off policy

--- app/src/main/java/com/example/asteroidradar/data/repository/WorkManagerRepository.kt
+++ app/src/main/java/com/example/asteroidradar/data/repository/WorkManagerRepository.kt
@@ -1,7 +1,11 @@
 package com.example.asteroidradar.data.repository
 
 import android.content.Context
+import androidx.work.BackoffPolicy
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
@@ -9,18 +13,33 @@ import com.example.asteroidradar.data.workers.FetchAsteroidsWorker
 import java.util.concurrent.TimeUnit
 
 interface WorkManagerRepository {
-    fun fetchAndSaveAsteroids()
+    fun periodically_fetch_asterpoids()
 }
 
 class WorkManagerRepositoryImpl(context: Context): WorkManagerRepository {
     private val workManager = WorkManager.getInstance(context)
-    val myWorkRequest = OneTimeWorkRequest.from(FetchAsteroidsWorker::class.java)
-    val saveRequest = PeriodicWorkRequestBuilder<FetchAsteroidsWorker>(
-        repeatInterval = 1,
-        repeatIntervalTimeUnit = TimeUnit.HOURS
-    )
 
-    override fun fetchAndSaveAsteroids() {
-        TODO("Not yet implemented")
+    override fun periodically_fetch_asterpoids() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(true)
+            .build()
+
+        val workRequest = PeriodicWorkRequestBuilder<FetchAsteroidsWorker>(
+            repeatInterval = 24,
+            repeatIntervalTimeUnit = TimeUnit.HOURS)
+            .setConstraints(constraints)
+            .setBackoffCriteria(
+                BackoffPolicy.EXPONENTIAL,
+                10, TimeUnit.MINUTES
+            ).build()
+
+        workManager.enqueueUniquePeriodicWork(
+            "unique_fetch_asteroids_worker",
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
+        )
     }
+
+
 }
