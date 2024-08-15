65ceac5e91009ef0e0e2e87594ad9cb6abfd264f
macky-boi <125427131+macky-boi@users.noreply.github.com>
2024-08-15 16:11:29 +0800

    NOT chained workers

--- app/src/androidTest/java/com/example/asteroidradar/UpdateAsteroidsWorkerInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/asteroidradar/UpdateAsteroidsWorkerInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package comprivate
+package com.example.asteroidradar
 
 import android.content.Context
 import android.util.Log
@@ -12,22 +12,17 @@ import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.testing.TestListenableWorkerBuilder
 import androidx.work.testing.WorkManagerTestInitHelper
-import com.example.asteroidradar.FETCH_ASTEROIDS_WORK_NAME
-import com.example.asteroidradar.FILTER_ASTEROIDS_WORK_NAME
-import com.example.asteroidradar.SAVE_ASTEROIDS_WORK_NAME
-import com.example.asteroidradar.UPDATE_ASTEROIDS_WORK_NAME
 import com.example.asteroidradar.data.workers.UpdateAsteroidsWorker
-import junit.framework.Assert.assertTrue
+import junit.framework.Assert
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import java.util.concurrent.TimeUnit
 
+private const val TAG = "UpdateAsteroidsWorkerInstrumentedTest"
 
-private const val TAG = "WorkManagerRepositoryInstrumentedTest"
-
-class WorkManagerRepositoryInstrumentedTest {
+class UpdateAsteroidsWorkerInstrumentedTest {
     private lateinit var context: Context
     private lateinit var workManager: WorkManager
 
@@ -46,8 +41,6 @@ class WorkManagerRepositoryInstrumentedTest {
             .setRequiredNetworkType(NetworkType.CONNECTED)
             .setRequiresCharging(true)
             .build()
-
-        val workRequest = TestListenableWorkerBuilder<UpdateAsteroidsWorker>(context).build()
 
         val periodicWorkRequest = PeriodicWorkRequestBuilder<UpdateAsteroidsWorker>(
             repeatInterval = 1,
@@ -70,16 +63,14 @@ class WorkManagerRepositoryInstrumentedTest {
 
             var workInfo = workManager.getWorkInfosForUniqueWork(UPDATE_ASTEROIDS_WORK_NAME).get()
             Log.i(TAG, "(BEFORE) workInfo: ${workInfo})")
-            assertTrue(workInfo.first().state == WorkInfo.State.ENQUEUED)
+            assert(workInfo.first().state == WorkInfo.State.ENQUEUED)
 
             val periodicWorkRequestId = workInfo.first().id
             testDriver.setAllConstraintsMet(periodicWorkRequestId)
-            testDriver.setPeriodDelayMet(periodicWorkRequestId)
 
             workInfo = workManager.getWorkInfosForUniqueWork(UPDATE_ASTEROIDS_WORK_NAME).get()
             Log.i(TAG, "(AFTER) workInfo: ${workInfo})")
-            assertTrue(workInfo.first().state == WorkInfo.State.RUNNING)
-
+            assert(workInfo.first().state == WorkInfo.State.RUNNING)
         }
     }
 }
--- app/src/main/java/com/example/asteroidradar/data/workers/UpdateAsteroidsWorker.kt
+++ app/src/main/java/com/example/asteroidradar/data/workers/UpdateAsteroidsWorker.kt
@@ -3,6 +3,7 @@ package com.example.asteroidradar.data.workers
 import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
+import androidx.work.ListenableWorker
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
@@ -12,40 +13,37 @@ import com.example.asteroidradar.AsteroidRadarApplication
 import com.example.asteroidradar.FETCH_ASTEROIDS_WORK_NAME
 import com.example.asteroidradar.FILTER_ASTEROIDS_WORK_NAME
 import com.example.asteroidradar.SAVE_ASTEROIDS_WORK_NAME
+import com.example.asteroidradar.data.remote.AsteroidNetwork
+import com.example.asteroidradar.data.remote.AsteroidsNetwork
+import retrofit2.HttpException
+import java.io.IOException
 
 private const val TAG = "UpdateAsteroidsWorker"
 
 class UpdateAsteroidsWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+    private val appContext = applicationContext as AsteroidRadarApplication
+    private val databaseRepository = appContext.container.asteroidDatabaseRepository
+    private val networkRepository = appContext.container.asteroidNetworkRepository
+
     override suspend fun doWork(): Result {
         Log.i("UpdateAsteroidsWorker", "doWork")
 
-        val appContext = applicationContext as AsteroidRadarApplication
-        val workManager = WorkManager.getInstance(appContext)
-
-        val filterWorkRequest = OneTimeWorkRequestBuilder<FilterAsteroidsWorker>()
-            .addTag(FILTER_ASTEROIDS_WORK_NAME)
-            .build()
-
-        val fetchWorkRequest = OneTimeWorkRequestBuilder<FetchAsteroidsWorker>()
-            .addTag(FETCH_ASTEROIDS_WORK_NAME)
-            .build()
-
-        val saveWorkRequest = OneTimeWorkRequestBuilder<SaveAsteroidsWorker>()
-            .addTag(SAVE_ASTEROIDS_WORK_NAME)
-            .build()
-
         return try {
-            val workContinuation = workManager.beginWith(filterWorkRequest)
-                .then(fetchWorkRequest)
-                .then(saveWorkRequest)
-
-            workContinuation.enqueue()
-
-            return Result.success()
+            databaseRepository.deleteAllAsteroidsFromThePast()
+            val fetchedData = networkRepository.fetchNearEarthObjects()
+            val asteroidsEntity = fetchedData.toEntity()
+            asteroidsEntity.forEach { (_, asteroid) ->
+                databaseRepository.insertAsteroids(asteroid)
+            }
+            Result.success()
+        } catch (httpException: HttpException) {
+            Log.e(TAG, "HTTP error: ${httpException.message}")
+            Result.failure()
         } catch (e: Exception) {
-            Log.e(TAG, "Error executing chain of workers: $e")
+            Log.e(TAG, "unexpected exception: ${e.message}")
             Result.failure()
         }
-
     }
+
+
 }
